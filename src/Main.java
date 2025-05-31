ffaf6f831ae85bb1e63822e73bcf53d103985065
Ilnaza Saifutdinova <ilnazasaifutdinova@gmail.com>
2025-05-31 22:19:29 +0200

    Add Welcome page for the Console App and exit option to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Gehtsoft Technical Assessment");
+
+        boolean key = true;
+        while(key) {
+            System.out.println("Please choose an option:");
+            System.out.println("1. Caesar Cipher Encryption");
+            System.out.println("2. Caesar Cipher Decryption");
+            System.out.println("3. Arithmetic Expression Evaluation");
+            System.out.println("4. Exit");
+            System.out.println("\nEnter your choice:");
+
+            String selectedNum = scanner.nextLine();
+
+            if (selectedNum.equals("4")) {
+                System.out.println("Goodbye!");
+                key = false;
+                break;
+            }
+
+            System.out.println("Option selected: " + selectedNum + " in progress");
+
+            System.out.println("\nContinue? y/n: ");
+            String check = scanner.nextLine();
+            if (check.equals("n")) {
+                key = false;
+                System.out.println("Goodbye!");
+            }
         }
+        scanner.close();
     }
 }
