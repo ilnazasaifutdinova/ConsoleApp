3b14aee10a82f0179e68d306a59c667d925bec46
Ilnaza Saifutdinova <ilnazasaifutdinova@gmail.com>
2025-06-01 22:02:47 +0200

    Implement Arithmetic Expression Evaluation

--- src/arithmetic/ArithmeticEval.java
+++ src/arithmetic/ArithmeticEval.java
@@ -23,12 +23,12 @@ public class ArithmeticEval {
 
             if (Character.isDigit(ch)) {
                 num.append(ch);
-            } else if (ch == '.' && num.length() > 0) {
+            } else if (ch == '.' && !num.isEmpty()) {
                 num.append(ch);
             } else if (ch == '-' && (i == 0 || isOperator(input.charAt(i - 1)) || input.charAt(i - 1) == '(')) {
                 num.append(ch); //minus for negative numbers
             } else {
-                if (num.length() > 0) {
+                if (!num.isEmpty()) {
                     tokenised.add(num.toString());
                     num.setLength(0);
                 }
@@ -39,7 +39,7 @@ public class ArithmeticEval {
                 }
             }
         }
-        if (num.length() > 0) {
+        if (!num.isEmpty()) {
             tokenised.add(num.toString());
         }
         return tokenised;
@@ -88,7 +88,42 @@ public class ArithmeticEval {
     }
 
     private long evalRNP(List<String> rpn) {
-        return Long.parseLong(rpn.get(0));
+        Stack<Double> stack = new Stack<>();
+
+        for (String token: rpn) {
+            if (token.matches("-?\\d+(\\.\\d+)?")) {
+                stack.push(Double.parseDouble(token));
+            } else if (isOperator(token.charAt(0))) {
+                if (stack.size() < 2) {
+                    throw new IllegalArgumentException("Invalid expression");
+                }
+                double oper1 = stack.pop();
+                double oper2 = stack.pop();
+                double expResult;
+                switch (token) {
+                    case "+":
+                        expResult = oper1 + oper2;
+                        break;
+                    case "-":
+                        expResult = oper2 - oper1;
+                        break;
+                    case "*":
+                        expResult = oper1 * oper2;
+                        break;
+                    case "/":
+                        if (oper2 == 0) throw new ArithmeticException("Division by zero");
+                        expResult = oper2 / oper1;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown operator: " + token);
+                }
+                stack.push(expResult);
+            }
+        }
+        if (stack.size() != 1) {
+            throw new IllegalArgumentException("Invalid expression");
+        }
+        return Math.round(stack.pop());
     }
 
     public long evaluate(String expression) {
